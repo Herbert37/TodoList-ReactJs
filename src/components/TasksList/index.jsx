69ee55c3eb7fe9aeae3e34c2b13c1b93cfd5ceac
Herbert Ayala <herbert.david37@gmail.com>
2021-06-15 11:12:55 -0500

    Bootstrap-design

--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -2,18 +2,26 @@ import React from "react";
 
 const TasksList = ({ taskList }) => {
   return (
-    <div>
-      {taskList.map((item, index) => (
-        <div key={index}>
-          <h1>{item.description}</h1>
-          {/* {<h2>{item.date}</h2>} */}
-          {item.isCompleted ? (
-            <h2>Task Completed</h2>
-          ) : (
-            <button>Complete task</button>
-          )}
-        </div>
-      ))}
+    <div class="row justify-content-center">
+      <div class="col-sm-6">
+        {taskList.map((item, index) => (
+          <div class="card" key={index}>
+            <div class="card-body">
+              <h5 class="card-title">{item.description}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{item.date}</h6>
+              {item.isCompleted ? (
+                <h6 class="card-subtitle mb-2 text-muted fst-italic">
+                  Task Completed
+                </h6>
+              ) : (
+                <button type="button" class="btn btn-outline-success">
+                  Complete task
+                </button>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
